907d341cadd8e3cd0645b6475e9b419867fdb0dc
Benjamín Vicente <benjavicente@uc.cl>
2023-03-20 12:08:30 -0300

    don't check code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,17 +8,17 @@ export type Props = {
 	exercises: string[];
 };
 
-function Exercise({ name, correctOutput }: { name: string; correctOutput: string[] }) {
+function Exercise({ name }: { name: string }) {
 	const pythonInstructorPromise = useMemo(createPythonInstructor, []);
 	const [code, setCode] = useState<string | undefined>("");
+	const [input, setInput] = useState<string | undefined>("");
 	const [status, setStatus] = useState({ state: "Esperando", stdout: [""], stderr: [""] });
 
 	const onRun = async () => {
 		const pythonInstructor = await pythonInstructorPromise;
 		setStatus({ state: "Corriendo", stdout: [], stderr: [] });
-		const result = await pythonInstructor.runCode({ file: code ?? "", input: [] });
-		const isCorrect = result.stdout.map((s) => s.trim()).join("\n") === correctOutput.map((s) => s.trim()).join("\n");
-		setStatus({ state: isCorrect ? "Funcionando!" : "Error", stdout: result.stdout, stderr: result.stderr });
+		const result = await pythonInstructor.runCode({ file: code ?? "", input: input?.split("\n") ?? [] });
+		setStatus({ state: result.error ? "Error" : "Ok", stdout: result.stdout, stderr: result.stderr });
 		console.log(result);
 	};
 
@@ -30,7 +30,7 @@ function Exercise({ name, correctOutput }: { name: string; correctOutput: string
 					<div
 						className={clsx(
 							"px-2 py-1 rounded-full",
-							status.state === "Funcionando!" ? "bg-success text-success-content" : "bg-error text-error-content"
+							status.state === "Ok" ? "bg-success text-success-content" : "bg-error text-error-content"
 						)}
 					>
 						{status.state}
@@ -42,26 +42,41 @@ function Exercise({ name, correctOutput }: { name: string; correctOutput: string
 					</button>
 				</div>
 			</summary>
-			<div className="p-4">
-				<Editor
-					height="30vh"
-					options={{ padding: { top: 8 } }}
-					defaultLanguage="python"
-					defaultValue=""
-					value={code}
-					theme="vs-dark"
-					onChange={setCode}
-				/>
+			<div className="p-4 flex flex-col gap-2">
+				<div className="mb-4">
+					<div className="p-2 font-bold text-lg">Input</div>
+					<Editor
+						height="20vh"
+						options={{ padding: { top: 8 } }}
+						defaultLanguage="textx"
+						defaultValue=""
+						value={input}
+						theme="vs-dark"
+						onChange={setInput}
+					/>
+				</div>
+				<div>
+					<div className="p-2 font-bold text-lg">Código</div>
+					<Editor
+						height="30vh"
+						options={{ padding: { top: 8 } }}
+						defaultLanguage="python"
+						defaultValue=""
+						value={code}
+						theme="vs-dark"
+						onChange={setCode}
+					/>
+				</div>
 				<details open className=" bg-base-200 rounded mt-4 border-base-300 border">
 					<summary className="marker:hidden list-none cursor-pointer bg-base-300 p-4">Resultado</summary>
 					<div className="flex flex-col gap-4 p-4">
 						<div>
-							Error
-							<pre className="bg-base-300 p-2 rounded-md min-h-16 overflow-y-scroll">{status.stderr.join("\n")}</pre>
-						</div>
-						<div>
 							Output
 							<pre className="bg-base-300 p-2 rounded-md min-h-16 overflow-y-scroll">{status.stdout.join("\n")}</pre>
+						</div>
+						<div>
+							Error
+							<pre className="bg-base-300 p-2 rounded-md min-h-16 overflow-y-scroll">{status.stderr.join("\n")}</pre>
 						</div>
 					</div>
 				</details>
@@ -72,8 +87,7 @@ function Exercise({ name, correctOutput }: { name: string; correctOutput: string
 export default function Home({ exercises }: Props) {
 	return (
 		<div className="px-4 py-8">
-			<h1 className="text-4xl font-bold text-center mb-5">Python</h1>
-			<Exercise name="Hola mundo" correctOutput={["Hola mundo"]} />
+			<Exercise name="DCCorredor" />
 		</div>
 	);
 }
